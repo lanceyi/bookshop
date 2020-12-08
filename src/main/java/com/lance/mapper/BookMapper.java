613bcac8051b2b11bb33d8979fd27a25c2b5bbc1
lanceyi <39644630+lanceyi@users.noreply.github.com>
2020-12-08 15:17:33 +0800

    v0.2

--- src/main/java/com/lance/mapper/BookMapper.java
+++ src/main/java/com/lance/mapper/BookMapper.java
@@ -10,15 +10,46 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface BookMapper {
+
+    /**
+     * 删除
+     * @param bookId
+     * @return int
+     */
     int deleteByPrimaryKey(Integer bookId);
 
+    /**
+     * 插入
+     * @param record
+     * @return int
+     */
     int insert(BookEntity record);
 
+    /**
+     * 选择插入
+     * @param record
+     * @return int
+     */
     int insertSelective(BookEntity record);
 
+    /**
+     * 主键查询
+     * @param bookId
+     * @return BookEntity
+     */
     BookEntity selectByPrimaryKey(Integer bookId);
 
+    /**
+     * 选择修改
+     * @param record
+     * @return int
+     */
     int updateByPrimaryKeySelective(BookEntity record);
 
+    /**
+     * 主键修改
+     * @param record
+     * @return int
+     */
     int updateByPrimaryKey(BookEntity record);
 }
--- src/main/java/com/lance/mapper/UserMapper.java
+++ src/main/java/com/lance/mapper/UserMapper.java
@@ -12,17 +12,59 @@ import java.util.List;
  */
 @Mapper
 public interface UserMapper {
+
+    /**
+     * 删除
+     * @param userId
+     * @return int
+     */
     int deleteByPrimaryKey(Integer userId);
 
+    /**
+     * 插入
+     * @param record
+     * @return int
+     */
     int insert(UserEntity record);
 
+    /**
+     * 查询所有
+     * @return List<UserEntity>
+     */
     List<UserEntity> selectAll ();
+
+    /**
+     * 根据用户名查询
+     * @param userName
+     * @return UserEntity
+     */
     UserEntity selectByName(String userName);
+
+    /**
+     * 选择插入
+     * @param record
+     * @return int
+     */
     int insertSelective(UserEntity record);
 
+    /**
+     * 主键查询
+     * @param userId
+     * @return UserEntity
+     */
     UserEntity selectByPrimaryKey(Integer userId);
 
+    /**
+     * 选择修改
+     * @param record
+     * @return int
+     */
     int updateByPrimaryKeySelective(UserEntity record);
 
+    /**
+     * 主键修改
+     * @param record
+     * @return int
+     */
     int updateByPrimaryKey(UserEntity record);
 }
